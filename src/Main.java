470bac334d812f96398009f7a54b48f039d03554
JuliaNikolajeva <sappfiire@gmail.com>
2023-09-17 23:18:26 +0300

    my first commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Say your name");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello," + name);
+        System.out.println("Today is a nice weather");
+
+
+    }
+
+
+
+
+
+}
