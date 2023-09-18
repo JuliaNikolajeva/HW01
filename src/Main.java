345614e8bba34faf7e57fb91a5ca5da96639be01
JuliaNikolajeva <sappfiire@gmail.com>
2023-09-18 14:23:38 +0300

    my homework (lesson1)

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello world!");
+
         Scanner scanner = new Scanner(System.in);
+        System.out.println("What your name?");
 
-        System.out.println("Say your name");
         String name = scanner.nextLine();
-
-        System.out.println("Hello," + name);
-        System.out.println("Today is a nice weather");
+        System.out.println("Welcome," + name);
 
 
     }
